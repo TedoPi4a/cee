a5b467c90032b70a4eaa2ec5437792b55ec9bde8
Teodor Angelov <teodorangelov152005@gmail.com>
2023-10-04 13:54:39 +0300

    files pdfs

---
+++ src/routes/news/BG14MFOP001-2.006/+page.svelte
@@ -0,0 +1,62 @@
+<h1 style="text-align: center; font-size: clamp(2.6rem, 8vw, 4rem);margin-top: 5vh">Новини</h1>
+<div class="text">
+    <h2 style="font-size: clamp(1.4em, 6vw, 2em) !important">BG16RFOP002-2.009 „Развитие на клъстери в България“</h2>
+    <h4 style="font-weight: 500; font-size: clamp(.9em, 4vw, 1em) !important">Крайният срок за подаване на проектни предложения пред Управляващия орган е 17:30 часа на 28.04.2017 г.</h4>
+    <p>Основната цел на процедурата е предоставяне на подкрепа за коопериране и създаване на клъстери в България, развитие на капацитета и интернационализацията на вече създадени такива като фактор за повишаване на конкурентоспособността и бизнес развитието.<br>
+    
+    По-долу ще намерите всички необходими документи за процедурата и за подготовка на документите за кандидатстване.</p>
+    <h4 style="font-weight: 500; font-size: clamp(.9em, 4vw, 1em) !important">Пакет документи за Процедура на подбор на проекти BG16RFOP002-2.009 „Развитие на клъстери в България“
+    </h4>
+    <p><a href="/documents/cluster_development/bg16rfop002-2.009.docx" target="_blank" rel="noopener noreferrer" download>Описание на процедурата</a></p>
+    <h5 style="font-size: 1.1em; font-weight: 400">Условия за изпълнение</h5>
+    <ul>
+        <li><a href="/documents/cluster_development/implementation_conditions.docx" target="_blank" rel="noopener noreferrer" download>Условия за изпълнение</a></li>
+        <li><a href="/documents/cluster_development/application_zh1.doc" target="_blank" rel="noopener noreferrer" download>Приложение Ж1</a> - Декларация за солидарна отговорност&nbsp;</li>
+        <li><a href="/documents/cluster_development/application_y.doc" target="_blank" rel="noopener noreferrer" download>Приложение Й</a> - Декларация за съгласие данните на кандидата да бъдат предоставени от НСИ на УО по служебен път</li>
+        <li><a href="/documents/cluster_development/application_k.doc" target="_blank" rel="noopener noreferrer" download>Приложение К</a> - Административен договор за безвъзмездна финансова помощ по Оперативна програма „Иновации и конкурентоспособност” 2014-2020</li>
+        <li><a href="/documents/cluster_development/application_l.doc" target="_blank" rel="noopener noreferrer" download>Приложение Л</a> - Общи условия към финансираните по Оперативна програма “Иновации и конкурентоспособност” 2014-2020 административни договори за предоставяне на безвъзмездна финансова помощ</li>
+        <li><a href="/documents/cluster_development/application_m.doc" target="_blank" rel="noopener noreferrer" download>Приложение М</a> - Декларация за нередности</li>
+        <li><a href="/documents/cluster_development/application_n.doc" target="_blank" rel="noopener noreferrer" download>Приложение Н</a> - Образец на одиторски доклад</li>
+        <li><a href="/documents/cluster_development/application_p.docx" target="_blank" rel="noopener noreferrer" download>Приложение П</a> - Заявление за профил за достъп на ръководител на бенефициента до ИСУН 2020</li>
+        <li><a href="/documents/cluster_development/application_r.docx" target="_blank" rel="noopener noreferrer" download>Приложение Р</a> - Заявление за профил за достъп на упълномощени от бенефициента лица до ИСУН 2020</li>
+        <li><a href="/documents/cluster_development/application_c.doc" target="_blank" rel="noopener noreferrer" download>Приложение Ц</a> - Декларация за участие в проектно предложение</li>
+        </ul>    
+        <h5 style="font-size: 1.1em; font-weight: 400">Условия за кандидатстване с проектни предложения</h5>
+        <p><a href="/documents/cluster_development/application_conditions.doc" target="_blank" rel="noopener noreferrer" download>Условия за кандидатстване</a></p>
+        <h5 style="font-size: 1.1em; font-weight: 400">Документи за информация</h5>
+        <ul>
+            <li><a href="/documents/cluster_development/application_a.doc" target="_blank" rel="noopener noreferrer" download>Приложение А </a>- Примерни указания за попълване на електронен Формуляр за кандидатстване</li>
+            <li><a href="/documents/cluster_development/application_b.doc" target="_blank" rel="noopener noreferrer" download>Приложение Б</a> - Методология за категоризиране на клъстери</li>
+            <li><a href="/documents/cluster_development/application_b1.docx" target="_blank" rel="noopener noreferrer" download>Приложение Б1</a> - Заявление за категоризиране на клъстер</li>
+            <li><a href="/documents/cluster_development/application_b2.doc" target="_blank" rel="noopener noreferrer" download>Приложение Б2</a> - Становище за категория клъстер</li>
+            <li><a href="/documents/cluster_development/application_b3.doc" target="_blank" rel="noopener noreferrer" download>Приложение Б3</a> - Контролен лист от проведено интервю</li>
+            <li><a href="/documents/cluster_development/application_i.doc" target="_blank" rel="noopener noreferrer" download>Приложение И</a> - Критерии и методология за оценка на проектните предложения</li>
+            <li><a href="/documents/cluster_development/application_s.doc" target="_blank" rel="noopener noreferrer" download>Приложение С</a> - Приложение I към Договора за създаване на европейската общност</li>
+            <li><a href="/documents/cluster_development/application_t.doc" target="_blank" rel="noopener noreferrer" download>Приложение Т</a> - Списък на общините в обхвата на селските райони на Република България</li>
+            <li><a href="/documents/cluster_development/application_u.pdf" target="_blank" rel="noopener noreferrer" download>Приложение У</a> - Класификация на икономическите дейности /КИД - 2008/</li>
+            <li><a href="/documents/cluster_development/application_f.docx" target="_blank" rel="noopener noreferrer" download>Приложение Ф</a> - Списък с регионите за планиране и областите, попадащи в тях</li>
+            <li><a href="/documents/cluster_development/application_h.docx" target="_blank" rel="noopener noreferrer" download>Приложение Х</a> - Използвани съкращения и основни дефиниции</li>
+            </ul>
+    </div>
+
+    <style>
+            .text{
+     width: 60em;
+        background-color: rgb(60, 60, 94);
+        border-radius: 1em;
+        padding: 1em 2em;
+     margin: 3em auto;
+     max-width: 95%;
+     box-sizing: border-box;
+    }
+    .text p{
+        font-weight: 300;
+        font-size: clamp(.9em, 4vw, 1.03em) !important
+    }
+    a{
+        color: #EDF7F6;
+    }
+    li{
+        line-height: 1.5em;
+    }
+    </style>
