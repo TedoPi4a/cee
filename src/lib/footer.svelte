9ce6fb2203db6c53fa31dc4b8b404d66da772ef1
TedoPi4a <teodorangelov152005gmail.com>
2023-12-12 01:19:56 +0200

    footer update

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -1,9 +1,20 @@
 <div>
-    <p style="margin: 0; padding-left: 1em; font-size: .8em; font-weight: 300; position:relative; bottom: 0">cee-bg @2023</p>
+    <p style="margin: 0; padding-left: 1em; font-size: clamp(.7rem, 2vw, .725rem); line-height: 1.45em; font-weight: 200; position:relative; bottom: 0">cee-bg @2023</p>
 </div>
 
 <style>
     div{
         background-color: #242236;
+    }
+
+    @media screen and (max-width: 699px) {
+        p{
+            margin: 0 auto !important;
+            padding: 0 !important;
+            width: fit-content;
+        }
+        div{
+            margin-top: 1em;
+        }
     }
 </style>
